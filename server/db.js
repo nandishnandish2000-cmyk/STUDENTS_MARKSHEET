b5ed9f7e9174af5770189cebe71de385d5dfb9b1
Nandish <nandish@example.com>
2026-02-03 12:44:57 +0530

    Prepared project for deployment (added dotenv and env variables)

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Nandish_16_',
-    database: 'marksheet_db',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'Nandish_16_',
+    database: process.env.DB_NAME || 'marksheet_db',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
